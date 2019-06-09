9ef6552ad6624544a697d08e7099e933f64d5f91
pgy <1104522530@qq.com>
2019-06-09 15:48:33 +0800

    注册了首页

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-      index
-      <el-button type='primary'>主要按钮</el-button>
-      <p>
-        <router-view></router-view>
-      </p>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -13,18 +9,6 @@ import axios from 'axios';
 
 export default {
   mounted(){
-
-    //请求测试接口
-    // axios({
-    //   url:'https://api.github.com/users',
-    //   method:'GET',
-    //   //get请求参数
-    //   params:{
-    //     abc:123
-    //   }
-    // }).then(res => {
-    //   console.log(res);
-    // })
 
     this.$axios({
       url: "https://api.github.com/users",
--- src/main.js
+++ src/main.js
@@ -1,29 +1,35 @@
 import Vue from 'vue';
 import App from './App.vue';
+import axios from 'axios';
+
 
 //element:1.导入组件
 import ElementUI from 'element-ui';
 //element的样式
 import 'element-ui/lib/theme-chalk/index.css';
 
-import axios from 'axios'
+// 导入组件
+import Login from './pages/Login.vue';
+import Admin from './pages/Admin.vue';
 
 import VueRouter from 'vue-router'
 
 // 注册路由
 Vue.use(VueRouter);
 
+//element:2.注册element插件
+Vue.use(ElementUI);
+
 const routes = [
-  //{ path:'/login',component:Login }
+  // { path:'/',redirect:'/admin' },
+  { path:'/login',component:Login },
+  { path:'/',component:Admin }
 ];
 
 // 路由实例
 const router = new VueRouter({ routes })
 
 Vue.prototype.$axios = axios;
-
-//element:2.注册element插件
-Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
